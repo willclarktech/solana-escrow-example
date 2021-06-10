8dbb3926cdbaa110b4500e844706e095c4add060
willclarktech <willclarktech@users.noreply.github.com>
2021-06-10 19:14:57 +0200

    Implement conversion from EscrowError to ProgramError

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,15 @@
 use thiserror::Error;
 
+use solana_program::program_error::ProgramError;
+
 #[derive(Clone, Copy, Debug, Error)]
 pub enum EscrowError {
 	#[error("Invalid Instruction")]
 	InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+	fn from(e: EscrowError) -> Self {
+		ProgramError::Custom(e as u32)
+	}
 }
